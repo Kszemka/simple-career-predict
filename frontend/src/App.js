eec0a34e38dc27d235c63bc265d5f1d3993c1bb7
tpeczkowsk <tomasz.peczkowski.tp@gmail.com>
2024-12-21 01:35:32 +0100

    Added error toasts
    - informowanie o bledzie pobierania pytan oraz wyslaniu zapytania do serwera
    - dodanie loadera informujacego o wysylaniu zapytania

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,25 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ResultCard from "./components/ResultCard";
 import StartCard from "./components/StartCard";
+import { ToastContainer, toast } from "react-toastify";
+
 function App() {
   const [questions, setQuestions] = useState([]);
   const [responses, setResponses] = useState({});
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [errorMessage, setErrorMessage] = useState(null);
   const [result, setResult] = useState("");
+  const [isPending, setIsPending] = useState(false);
   const [step, setStep] = useState("start"); // "start", "quiz", "result"
 
   const scale = [1, 2, 3, 4, 5]; // Scale for responses
 
   useEffect(() => {
-    setErrorMessage(null);
     fetchQuestions();
   }, []);
 
   const fetchQuestions = () => {
+    setErrorMessage(null);
     fetch("/questions.json")
       .then((response) => {
         if (!response.ok) {
@@ -58,11 +61,14 @@ function App() {
       return acc;
     }, {});
 
+    setIsPending(true);
+    setErrorMessage(null);
+
     questions.forEach(({ category }, index) => {
       formattedResponses[category].push(responses[index]);
     });
 
-    fetch("/submit_scores", {
+    await fetch("/submit_scores", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -81,7 +87,12 @@ function App() {
       })
       .catch((error) => {
         console.error("Error submitting scores:", error);
+        toast.error("Błąd połączenia z serwerem. Spróbuj ponownie.", {
+          position: "top-right",
+          theme: "colored",
+        });
       });
+    setIsPending(false);
     // setResult("Zawód rodziców");
     // setStep("result");
   };
@@ -107,75 +118,87 @@ function App() {
       {step === "start" && <StartCard click={() => setStep("quiz")} error={errorMessage} />}
       {step === "result" && <ResultCard result={result} handleRestart={handleRestart} />}
       {step === "quiz" && (
-        <div className="App">
-          <div className="container mt-5 d-flex flex-column justify-content-center align-items-center">
-            <h1 className="text-center">Inteligentny Asystent do predykcji najbardziej odpowiedniego zawodu w oparciu o testy osobowości</h1>
-            <div className="card p-4 mt-5 shadow border border-0" style={{ width: "50rem" }}>
-              <div className="card-body">
-                <h6 className="d-flex pb-5">
-                  <div>Pytanie</div>
-                  <div className="text-center" style={{ width: "2rem" }}>
-                    {currentQuestionIndex + 1}
-                  </div>
-                  /
-                  <div className="text-center" style={{ width: "2rem" }}>
-                    {questions.length}
-                  </div>
-                </h6>
-                <h5 className="text-center pb-5">{currentQuestion.question}</h5>
-                <form className="pb-2">
-                  <div className="d-flex align-items-center justify-content-center gap-4">
-                    <label className="text-right" style={{ width: "120px" }}>
-                      Nie zgadzam się
-                    </label>
-                    <div className="btn-group" role="group">
-                      {scale.map((number) => (
-                        <React.Fragment key={number}>
-                          <input
-                            type="radio"
-                            name={`question-${currentQuestionIndex}`}
-                            className="btn-check"
-                            id={`option-${number}`}
-                            value={number}
-                            autoComplete="off"
-                            checked={responses[currentQuestionIndex] === number}
-                            onChange={() => handleRadioChange(currentQuestionIndex, number)}
-                          />
-                          <label className="btn btn-outline-primary" htmlFor={`option-${number}`}>
-                            {number}
-                          </label>
-                        </React.Fragment>
-                      ))}
+        <>
+          <div className="App">
+            <div className="container mt-5 d-flex flex-column justify-content-center align-items-center">
+              <h1 className="text-center">Inteligentny Asystent do predykcji najbardziej odpowiedniego zawodu w oparciu o testy osobowości</h1>
+              <div className="card p-4 mt-5 shadow border border-0" style={{ width: "50rem" }}>
+                <div className="card-body">
+                  <h6 className="d-flex pb-5">
+                    <div>Pytanie</div>
+                    <div className="text-center" style={{ width: "2rem" }}>
+                      {currentQuestionIndex + 1}
                     </div>
-                    <label className="text-left" style={{ width: "120px" }}>
-                      Zgadzam się
-                    </label>
-                  </div>
-                </form>
-                <div className="d-flex justify-content-between pt-3">
-                  <button className="btn btn-secondary" onClick={handlePrevious} disabled={currentQuestionIndex === 0}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-arrow-left-circle-fill" viewBox="0 0 18 18">
-                      <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z" />
-                    </svg>
-                    <span className="mx-2">Poprzednie</span>
-                  </button>
-                  {currentQuestionIndex < questions.length - 1 ? (
-                    <button className="btn btn-primary" onClick={handleNext} disabled={responses[currentQuestionIndex] === null}>
-                      <span className="mx-2">Następne</span>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-arrow-right-circle-fill" viewBox="0 0 18 18">
-                        <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
+                    /
+                    <div className="text-center" style={{ width: "2rem" }}>
+                      {questions.length}
+                    </div>
+                  </h6>
+                  <h5 className="text-center pb-5">{currentQuestion.question}</h5>
+                  <form className="pb-2">
+                    <div className="d-flex align-items-center justify-content-center gap-4">
+                      <label className="text-right" style={{ width: "120px" }}>
+                        Nie zgadzam się
+                      </label>
+                      <div className="btn-group" role="group">
+                        {scale.map((number) => (
+                          <React.Fragment key={number}>
+                            <input
+                              type="radio"
+                              name={`question-${currentQuestionIndex}`}
+                              className="btn-check"
+                              id={`option-${number}`}
+                              value={number}
+                              autoComplete="off"
+                              checked={responses[currentQuestionIndex] === number}
+                              onChange={() => handleRadioChange(currentQuestionIndex, number)}
+                            />
+                            <label className="btn btn-outline-primary" htmlFor={`option-${number}`}>
+                              {number}
+                            </label>
+                          </React.Fragment>
+                        ))}
+                      </div>
+                      <label className="text-left" style={{ width: "120px" }}>
+                        Zgadzam się
+                      </label>
+                    </div>
+                  </form>
+                  <div className="d-flex justify-content-between pt-3">
+                    <button className="btn btn-secondary" onClick={handlePrevious} disabled={currentQuestionIndex === 0}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-arrow-left-circle-fill" viewBox="0 0 18 18">
+                        <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z" />
                       </svg>
+                      <span className="mx-2">Poprzednie</span>
                     </button>
-                  ) : (
-                    <button className="btn btn-success" onClick={handleSubmit} disabled={!allAnswered}>
-                      Wyślij
-                    </button>
-                  )}
+                    {currentQuestionIndex < questions.length - 1 ? (
+                      <button className="btn btn-primary" onClick={handleNext} disabled={responses[currentQuestionIndex] === null}>
+                        <span className="mx-2">Następne</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-arrow-right-circle-fill" viewBox="0 0 18 18">
+                          <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
+                        </svg>
+                      </button>
+                    ) : (
+                      <button className="btn btn-success text-center" style={{ width: "8rem" }} onClick={handleSubmit} disabled={!allAnswered || isPending}>
+                        {isPending ? (
+                          <div className="spinner-grow text-light" style={{ width: "1rem", height: "1rem" }} role="status"></div>
+                        ) : (
+                          <div>
+                            <span className="mx-2">Wyślij</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-send-fill" viewBox="0 0 18 18">
+                              <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z" />
+                            </svg>
+                          </div>
+                        )}
+                      </button>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+          <ToastContainer />
+        </>
       )}
     </>
   );
--- frontend/src/components/StartCard.js
+++ frontend/src/components/StartCard.js
@@ -1,23 +1,42 @@
+import { ToastContainer, toast } from "react-toastify";
+import { useEffect } from "react";
+
 export default function StartCard({ click, error }) {
+  useEffect(() => {
+    if (error) {
+      notify();
+    }
+  }, [error]);
+
+  const notify = () => {
+    toast.error(`${error}. Odśwież stronę.`, {
+      position: "top-right",
+      theme: "colored",
+    });
+  };
+
   return (
-    <div className="app">
-      <div className="container mt-5 text-center d-flex flex-column justify-content-center align-items-center">
-        <h1>Inteligentny Asystent do predykcji najbardziej odpowiedniego zawodu w oparciu o testy osobowości</h1>
-        <div className="card p-4 mt-5 shadow border border-0" style={{ width: "50rem" }}>
-          <div className="card-body">
-            <h5 className="pt-2 mb-4">O projekcie</h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo quam, eleifend vitae sapien sed, porttitor porta quam. Integer tortor metus, venenatis sit amet ligula ac, interdum
-              cursus ante. Cras quis ante eget erat placerat varius a sit amet quam.
-            </p>
-            <div className="mt-5">
-              <button className="btn btn-primary btn-lg" onClick={click}>
-                Rozpocznij test
-              </button>
+    <>
+      <div className="app">
+        <div className="container mt-5 text-center d-flex flex-column justify-content-center align-items-center">
+          <h1>Inteligentny Asystent do predykcji najbardziej odpowiedniego zawodu w oparciu o testy osobowości</h1>
+          <div className="card p-4 mt-5 shadow border border-0" style={{ width: "50rem" }}>
+            <div className="card-body">
+              <h5 className="pt-2 mb-4">O projekcie</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo quam, eleifend vitae sapien sed, porttitor porta quam. Integer tortor metus, venenatis sit amet ligula ac, interdum
+                cursus ante. Cras quis ante eget erat placerat varius a sit amet quam.
+              </p>
+              <div className="mt-5">
+                <button className="btn btn-primary btn-lg" onClick={click} disabled={!!error}>
+                  Rozpocznij test
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <ToastContainer />
+    </>
   );
 }
